d242255726816155ec4569667a530da2c4c5c820
dos54 <stevencthomas54@gmail.com>
2024-11-09 13:23:32 -0900

    Completed 'code' assignment

--- week01/analyze/Sorting.cs
+++ week01/analyze/Sorting.cs
@@ -5,9 +5,11 @@
         Console.Out.WriteLine("int[]{{{0}}}", string.Join(", ", numbers)); //int[]{1, 2, 3, 4, 6, 8, 9}
     }
 
+    // O (n^2)
     private static void SortArray(int[] data) {
+        // Start at the end of the array.
         for (var sortPos = data.Length - 1; sortPos >= 0; sortPos--) {
-            for (var swapPos = 0; swapPos < sortPos; ++swapPos) {
+            for (var swapPos = 0; swapPos < sortPos; ++swapPos) { // For each item start at the beginning and see if it's smaller than the item at the end of the array
                 if (data[swapPos] > data[swapPos + 1]) {
                     (data[swapPos + 1], data[swapPos]) = (data[swapPos], data[swapPos + 1]);
                 }
--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,14 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // I will create an empty fixed array of size 'length'. I will run a for loop starting at 0 that multiplies 'number' by the current number
+        // of iterations 'i' + 1. Set the value at index 'i' of the 'multiples' array to the result.
+        double[] multiples = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = number * (i + 1);
+        }
+        return multiples; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +36,18 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // I will take all of the values AFTER the 'amount' (data.Count - amount) and copy those to
+        // a new, temporary list. I will take all the values BEFORE that and paste them to another
+        // temporary list. I will then clear the 'data' list and add first the last few items,
+        // then the first few items.
+        
+        // Essentially, cut the original array into 2 slices. Hold each slice in temporary arrays.
+        // Clear the original array, then paste the 2 slices into the original array at opposite sides.  
+        List<int> afterCutoff = data.GetRange(data.Count - amount, amount);
+        List<int> beforeCutoff = data.GetRange(0, data.Count - amount);
+        data.Clear();
+        data.AddRange(afterCutoff);
+        data.AddRange(beforeCutoff);
     }
 }
